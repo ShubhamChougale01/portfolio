3d4144c03c99f99d4ec905e6aa7c945f2907dbaf
ShubhamChougale01 <shubham.chougale001@gmail.com>
2025-07-15 15:36:04 +0530

    chnage in resume

--- backend_p/backend_p/settings.py
+++ backend_p/backend_p/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&bp8mnoxtt#&@j8=_!$l8hydsl(1f=l=p$#&+xbnym^9a^b)14'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
