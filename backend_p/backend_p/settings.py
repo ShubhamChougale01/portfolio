03938b423b5513e4eaeee654e4ae8bd1e53da197
ShubhamChougale01 <shubham.chougale001@gmail.com>
2025-07-15 11:45:57 +0530

    Added Vercel frontend URLs to CORS_ALLOWED_ORIGINS

--- backend_p/backend_p/settings.py
+++ backend_p/backend_p/settings.py
@@ -44,15 +44,20 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://portfolio-frontend-7qkq4q1fk-shubham-s-projects-b0658d3e.vercel.app",
+    "https://portfolio-frontend-six-self.vercel.app",  
 ]
 
 ROOT_URLCONF = 'backend_p.urls'
@@ -74,8 +79,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend_p.wsgi.application'
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# This setting informs Django of the URI path from which your static files will be served to users
+# Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
+STATIC_URL = '/static/'
+
+# This production code might break development mode, so we check whether we're in DEBUG mode
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 # Database
@@ -122,9 +138,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'dummy-insecure-dev-key')
 # Default primary key field type
